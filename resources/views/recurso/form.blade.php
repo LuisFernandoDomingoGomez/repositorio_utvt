da9f0a56cdca1143a08a05eea03a71028781b528
Luis Fernando Domingo Gómez <al222010066@gmail.com>
2023-10-17 14:46:40 -0600

    Form Recursos - Pt1

--- resources/views/recurso/form.blade.php
+++ resources/views/recurso/form.blade.php
@@ -1,77 +1,130 @@
-<div class="box box-info padding-1">
+<div class="box box-info p-4">
     <div class="box-body">
-        
-        <div class="form-group">
-            {{ Form::label('titulo') }}
-            {{ Form::text('titulo', $recurso->titulo, ['class' => 'form-control' . ($errors->has('titulo') ? ' is-invalid' : ''), 'placeholder' => 'Titulo']) }}
-            {!! $errors->first('titulo', '<div class="invalid-feedback">:message</div>') !!}
+        <div class="form-group mb-4">
+            {{ Form::label('titulo', 'Título', ['class' => 'block text-gray-700 text-sm font-bold mb-2']) }}
+            {{ Form::text('titulo', $recurso->titulo, ['class' => 'form-input w-full rounded-md focus:outline-none focus:ring focus:border-blue-300' . ($errors->has('titulo') ? ' border-red-500' : ''), 'placeholder' => 'Título']) }}
+            {!! $errors->first('titulo', '<p class="text-red-500 text-xs mt-1">:message</p>') !!}
+        </div>
+        <div class="grid grid-cols-3 gap-4">
+            <div class="form-group">
+                {{ Form::label('carrera_id', 'Carrera', ['class' => 'block text-gray-700 text-sm font-bold mb-2']) }}
+                {{ Form::select('carrera_id', $carrera, $recurso->carrera_id, ['class' => 'form-select w-full rounded-md focus:outline-none focus:ring focus:border-blue-300' . ($errors->has('carrera_id') ? ' border-red-500' : ''), 'placeholder' => 'Selecciona una Carrera']) }}
+                {!! $errors->first('carrera_id', '<p class="text-red-500 text-xs mt-1">:message</p>') !!}
+            </div>
+
+            <div class="form-group">
+                {{ Form::label('asignatura_id', 'Asignatura', ['class' => 'block text-gray-700 text-sm font-bold mb-2']) }}
+                {{ Form::select('asignatura_id', $asignatura, $recurso->asignatura_id, ['class' => 'form-select w-full rounded-md focus:outline-none focus:ring focus:border-blue-300' . ($errors->has('asignatura_id') ? ' border-red-500' : ''), 'placeholder' => 'Selecciona una Asignatura']) }}
+                {!! $errors->first('asignatura_id', '<p class="text-red-500 text-xs mt-1">:message</p>') !!}
+            </div>
+
+            <div class="form-group">
+                {{ Form::label('tematica_id', 'Tematica', ['class' => 'block text-gray-700 text-sm font-bold mb-2']) }}
+                {{ Form::select('tematica_id', $tematica, $recurso->tematica_id, ['class' => 'form-select w-full rounded-md focus:outline-none focus:ring focus:border-blue-300' . ($errors->has('tematica_id') ? ' border-red-500' : ''), 'placeholder' => 'Selecciona una Tematica']) }}
+                {!! $errors->first('tematica_id', '<p class="text-red-500 text-xs mt-1">:message</p>') !!}
+            </div>
         </div>
         <br>
-        <div class="form-group">
-            {{ Form::label('descripcion') }}
-            {{ Form::text('descripcion', $recurso->descripcion, ['class' => 'form-control' . ($errors->has('descripcion') ? ' is-invalid' : ''), 'placeholder' => 'Descripcion']) }}
-            {!! $errors->first('descripcion', '<div class="invalid-feedback">:message</div>') !!}
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            <div class="form-group col-span-2">
+                {{ Form::label('descripcion', 'Descripción', ['class' => 'block text-gray-700 text-sm font-bold mb-2']) }}
+                {{ Form::textarea('descripcion', $recurso->descripcion, ['class' => 'form-input w-full rounded-md focus:outline-none focus:ring focus:border-blue-300' . ($errors->has('descripcion') ? ' border-red-500' : ''), 'placeholder' => 'Descripción', 'rows' => 3]) }}
+                {!! $errors->first('descripcion', '<p class="text-red-500 text-xs mt-1">:message</p>') !!}
+            </div>
+
+            <div class="form-group col-span-2">
+                {{ Form::label('archivo', 'Subir archivo', ['class' => 'block text-gray-700 text-sm font-bold mb-2']) }}
+                {{ Form::file('archivo', ['class' => 'hidden', 'id' => 'file-input']) }}
+                <div id="file-drop" class="w-full h-60 border-dashed border-2 border-gray-300 rounded-lg flex flex-col items-center justify-center cursor-pointer">
+                    <svg class="w-12 h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M22 8v12h-5v12h-4l11 10 11-10h-4V20h-5V8z"></path>
+                    </svg>
+                    <p class="text-gray-400 text-sm md:text-base lg:text-lg mt-2">Arrastra y suelta archivos aquí o haz clic para seleccionar</p>
+                </div>
+                <p id="file-name" class="mt-2 text-gray-700 text-sm"></p>
+            </div>
         </div>
-        <br>
+
+
         <div class="form-group">
-            {{ Form::label('Carrera') }}
-            {{ Form::select('carrera_id', $carrera, $recurso->carrera_id, ['class' => 'form-control' . ($errors->has('carrera_id') ? ' is-invalid' : ''), 'placeholder' => 'Selecciona una Carrera']) }}
-            {!! $errors->first('carrera_id', '<div class="invalid-feedback">:message</div>') !!}
+            {{ Form::label('tipo', 'Tipo', ['class' => 'block text-gray-700 text-sm font-bold mb-2']) }}
+            {{ Form::text('tipo', $recurso->tipo, ['class' => 'form-input w-full rounded-md focus:outline-none focus:ring focus:border-blue-300' . ($errors->has('tipo') ? ' border-red-500' : ''), 'placeholder' => 'Tipo']) }}
+            {!! $errors->first('tipo', '<p class="text-red-500 text-xs mt-1">:message</p>') !!}
         </div>
-        <br>
+
         <div class="form-group">
-            {{ Form::label('Asignatura') }}
-            {{ Form::select('asignatura_id', $asignatura, $recurso->asignatura_id, ['class' => 'form-control' . ($errors->has('asignatura_id') ? ' is-invalid' : ''), 'placeholder' => 'Selecciona una Asignatura']) }}
-            {!! $errors->first('asignatura_id', '<div class="invalid-feedback">:message</div>') !!}
+            {{ Form::label('autor', 'Autor', ['class' => 'block text-gray-700 text-sm font-bold mb-2']) }}
+            {{ Form::text('autor', $recurso->autor, ['class' => 'form-input w-full rounded-md focus:outline-none focus:ring focus:border-blue-300' . ($errors->has('autor') ? ' border-red-500' : ''), 'placeholder' => 'Autor']) }}
+            {!! $errors->first('autor', '<p class="text-red-500 text-xs mt-1">:message</p>') !!}
         </div>
-        <br>
+
         <div class="form-group">
-            {{ Form::label('Tematica') }}
-            {{ Form::select('tematica_id', $tematica, $recurso->tematica_id, ['class' => 'form-control' . ($errors->has('tematica_id') ? ' is-invalid' : ''), 'placeholder' => 'Selecciona una Categoria']) }}
-            {!! $errors->first('tematica_id', '<div class="invalid-feedback">:message</div>') !!}
+            {{ Form::label('user_id', 'User Id', ['class' => 'block text-gray-700 text-sm font-bold mb-2']) }}
+            {{ Form::text('user_id', $recurso->user_id, ['class' => 'form-input w-full rounded-md focus:outline-none focus:ring focus:border-blue-300' . ($errors->has('user_id') ? ' border-red-500' : ''), 'placeholder' => 'User Id']) }}
+            {!! $errors->first('user_id', '<p class="text-red-500 text-xs mt-1">:message</p>') !!}
         </div>
-        <br>
+
         <div class="form-group">
-            {{ Form::label('autor') }}
-            {{ Form::text('autor', $recurso->autor, ['class' => 'form-control' . ($errors->has('autor') ? ' is-invalid' : ''), 'placeholder' => 'Autor']) }}
-            {!! $errors->first('autor', '<div class="invalid-feedback">:message</div>') !!}
+            {{ Form::label('anonimo', 'Anónimo', ['class' => 'block text-gray-700 text-sm font-bold mb-2']) }}
+            {{ Form::text('anonimo', $recurso->anonimo, ['class' => 'form-input w-full rounded-md focus:outline-none focus:ring focus:border-blue-300' . ($errors->has('anonimo') ? ' border-red-500' : ''), 'placeholder' => 'Anónimo']) }}
+            {!! $errors->first('anonimo', '<p class="text-red-500 text-xs mt-1">:message</p>') !!}
         </div>
-        <br>
+
         <div class="form-group">
-            {{ Form::label('user_id') }}
-            {{ Form::text('user_id', $recurso->user_id, ['class' => 'form-control' . ($errors->has('user_id') ? ' is-invalid' : ''), 'placeholder' => 'User Id']) }}
-            {!! $errors->first('user_id', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <br>
-        <div class="form-group">
-            {{ Form::label('anonimo') }}
-            {{ Form::text('anonimo', $recurso->anonimo, ['class' => 'form-control' . ($errors->has('anonimo') ? ' is-invalid' : ''), 'placeholder' => 'Anonimo']) }}
-            {!! $errors->first('anonimo', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <br>
-        <div class="form-group">
-            {{ Form::label('archivo') }}
-            {{ Form::text('archivo', $recurso->archivo, ['class' => 'form-control' . ($errors->has('archivo') ? ' is-invalid' : ''), 'placeholder' => 'Archivo']) }}
-            {!! $errors->first('archivo', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <br>
-        <div class="form-group">
-            {{ Form::label('tipo') }}
-            {{ Form::text('tipo', $recurso->tipo, ['class' => 'form-control' . ($errors->has('tipo') ? ' is-invalid' : ''), 'placeholder' => 'Tipo']) }}
-            {!! $errors->first('tipo', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <br>
-        <div class="form-group">
-            {{ Form::label('estado') }}
-            {{ Form::text('estado', $recurso->estado, ['class' => 'form-control' . ($errors->has('estado') ? ' is-invalid' : ''), 'placeholder' => 'Estado']) }}
-            {!! $errors->first('estado', '<div class="invalid-feedback">:message</div>') !!}
+            {{ Form::label('estado', 'Estado', ['class' => 'block text-gray-700 text-sm font-bold mb-2']) }}
+            {{ Form::text('estado', $recurso->estado, ['class' => 'form-input w-full rounded-md focus:outline-none focus:ring focus:border-blue-300' . ($errors->has('estado') ? ' border-red-500' : ''), 'placeholder' => 'Estado']) }}
+            {!! $errors->first('estado', '<p class="text-red-500 text-xs mt-1">:message</p>') !!}
         </div>
     </div>
-    <br>
-    <div class="box-footer mt20">
+    <div class="box-footer mt-4">
         <div class="float-right">
             <a href="{{ route('recursos.index') }}" class="btn btn-danger mr-2">{{ __('Regresar') }}</a>
             <button type="submit" class="btn btn-primary">{{ __('Guardar') }}</button>
         </div>
     </div>
 </div>
+
+<script>
+    const fileDrop = document.getElementById('file-drop');
+    const fileInput = document.getElementById('file-input');
+    const fileNameDisplay = document.getElementById('file-name');
+
+    fileDrop.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        fileDrop.classList.add('border-blue-500');
+    });
+
+    fileDrop.addEventListener('dragleave', () => {
+        fileDrop.classList.remove('border-blue-500');
+    });
+
+    fileDrop.addEventListener('drop', (e) => {
+        e.preventDefault();
+        fileDrop.classList.remove('border-blue-500');
+
+        const files = e.dataTransfer.files;
+        handleFiles(files);
+    });
+
+    fileDrop.addEventListener('click', () => {
+        fileInput.click();
+    });
+
+    fileInput.addEventListener('change', () => {
+        const files = fileInput.files;
+        handleFiles(files);
+    });
+
+    function handleFiles(files) {
+        for (const file of files) {
+            // Muestra el nombre del archivo seleccionado
+            fileNameDisplay.textContent = 'Archivo seleccionado: ' + file.name;
+
+            // Aquí puedes procesar el archivo y/o enviarlo al servidor según tus necesidades.
+            // Por ejemplo, subir el archivo al servidor.
+            // const formData = new FormData();
+            // formData.append('archivo', file);
+            // Realizar una solicitud AJAX o utilizar fetch() para enviar el archivo al servidor.
+        }
+    }
+</script>
