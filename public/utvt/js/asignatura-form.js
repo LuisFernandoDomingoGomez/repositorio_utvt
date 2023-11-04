5bdba8c039dd982ee18e12185409bb68b1d986a6
Karla Rodriguez Beltran <al222010231@gmail.com>
2023-11-04 01:15:02 -0600

    Optimización de Styles 3

---
+++ public/utvt/js/asignatura-form.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    const imagenDrop = document.getElementById('imagen-drop');
+    const imagenInput = document.getElementById('imagen-input');
+
+    imagenDrop.addEventListener('click', () => {
+        imagenInput.click();
+    });
+
+    imagenInput.addEventListener('change', (e) => {
+        const file = e.target.files[0];
+        displayImage(file);
+    });
+
+    imagenDrop.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        imagenDrop.classList.add('border-blue-500');
+    });
+
+    imagenDrop.addEventListener('dragleave', (e) => {
+        e.preventDefault();
+        imagenDrop.classList.remove('border-blue-500');
+    });
+
+    imagenDrop.addEventListener('drop', (e) => {
+        e.preventDefault();
+        imagenDrop.classList.remove('border-blue-500');
+        const file = e.dataTransfer.files[0];
+        displayImage(file);
+    });
+
+    function displayImage(file) {
+        if (file) {
+            const imagenName = document.getElementById('imagen-name');
+            imagenName.textContent = file.name;
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                // Muestra la imagen
+                const img = new Image();
+                img.src = e.target.result;
+                img.classList.add('w-32', 'h-32', 'rounded-md', 'shadow-md');
+                imagenDrop.innerHTML = '';
+                imagenDrop.appendChild(img);
+            };
+            reader.readAsDataURL(file);
+        }
+    }
+});
